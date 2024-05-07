8ccbaa7acf8ee813227f0f51c97d28c5a2adfd18
Sanskar-12 <sanskarv2004@gmail.com>
2024-05-07 11:01:22 +0530

    arrays

---
+++ array/array.ts
@@ -0,0 +1,15 @@
+// First Representation
+let myArr: string[] = [];
+myArr.push("ironman");
+
+// Second Representation
+let meraArr: Array<string> = [];
+meraArr.push("Superman");
+
+// Another example
+type User = {
+  name: string;
+  isActive: boolean;
+};
+
+let userArr: User[] = [];
