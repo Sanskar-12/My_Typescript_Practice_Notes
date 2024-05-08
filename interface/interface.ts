df462b9b263b3da33cb0ba53864422c8d80ace0f
Sanskar-12 <sanskarv2004@gmail.com>
2024-05-08 11:47:33 +0530

    interface

---
+++ interface/interface.ts
@@ -0,0 +1,21 @@
+interface User {
+  readonly dbId: string;
+  email: string;
+  userId: number;
+  googleId?: string;
+  startTrial: () => string;
+  getCoupon: (couponname: string) => number;
+}
+
+const user: User = {
+  dbId: "sdfsdf",
+  email: "s@gmail.com",
+  userId: 23,
+  googleId: "sdfsdf",
+  startTrial: () => {
+    return "sanskar";
+  },
+  getCoupon: (couponname = "sdsd") => {
+    return 10;
+  },
+};
