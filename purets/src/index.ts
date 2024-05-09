6ec0fd364a41b4606f2074492d3a573b5e7afea7
Sanskar-12 <sanskarv2004@gmail.com>
2024-05-09 12:35:44 +0530

    ts with html

---
+++ purets/src/index.ts
@@ -0,0 +1,6 @@
+// tsc --init ---> for generating the tsconfig.json
+// change the rootDir and outDir in tsconfig.json file
+// write the code in ts file
+// tsc -w to continuosly watch and compile the ts file
+
+console.log("Chai aur code");
