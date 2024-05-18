973892d9775923b3d5aae3e85006409146fe13e6
Sanskar-12 <sanskarv2004@gmail.com>
2024-05-18 15:52:45 +0530

    Protected variables

--- purets/src/index.ts
+++ purets/src/index.ts
@@ -3,9 +3,13 @@
 // write the code in ts file
 // tsc -w to continuosly watch and compile the ts file
 
+// ----------------------------------------------------------------------------------
+
 // Starting with Ts
 
 // console.log("Chai aur code");
+
+// ----------------------------------------------------------------------------------
 
 // Classes in Ts
 
@@ -21,6 +25,8 @@
 
 // const user = new User("sanskar", "s@g.com");
 // user.city="Mumbai"
+
+// ----------------------------------------------------------------------------------
 
 // Private keyword in Ts
 // Private keywords can be accessed within the class not outside it
@@ -39,6 +45,8 @@
 // const user = new User("sanskar", "s@g.com");
 // user.city="Mumbai"  ---> This will give me error because i am accessing it outside the User class
 
+// ----------------------------------------------------------------------------------
+
 // Public keyword in Ts
 // Public keywords can be accessed within the class or outside it
 // class User {
@@ -55,10 +63,33 @@
 // const user = new User("sanskar", "s@g.com");
 // user.city = "Mumbai"; // Public
 
+// ----------------------------------------------------------------------------------
+
 // Getter Functions is used to get the private variables outside the class and Setter Functions are used to set the values for private varaibles
 
+// class User {
+//   private courseId: number;
+//   constructor(courseId: number) {
+//     this.courseId = courseId;
+//   }
+
+//   get getCourseId(): number {
+//     return this.courseId;
+//   }
+
+//   set setCourseCount(courseNum: number) {
+//     this.courseId = courseNum;
+//   }
+// }
+
+// const myObject = new User(3434);
+// myObject.getCourseId;
+
+// ----------------------------------------------------------------------------------
+
+// Protected variables are only accessed inside the class or inside the class which extends the parent class
 class User {
-  private courseId: number;
+  protected courseId: number;
   constructor(courseId: number) {
     this.courseId = courseId;
   }
@@ -72,5 +103,13 @@ class User {
   }
 }
 
-const myObject = new User(3434);
+class FamilyUser extends User {
+  isFamily: boolean = true;
+
+  changeCourseId() {
+    this.courseId = 2323;
+  }
+}
+
+const myObject = new FamilyUser(3434);
 myObject.getCourseId;
