b882dad6a4d1af1cf9c1240d2d2dba903349b496
Sanskar-12 <sanskarv2004@gmail.com>
2024-05-19 17:57:43 +0530

    Generics with array and arrow functions

--- purets2/src/generics.ts
+++ purets2/src/generics.ts
@@ -1,27 +1,39 @@
-//  Generics can be used to make reusable components, its like any but any can accept any type of val and return any type of value but Generics only return one Type
+// //  Generics can be used to make reusable components, its like any but any can accept any type of val and return any type of value but Generics only return one Type
 
-function identityOne(val: number | boolean): number | boolean {
+// function identityOne(val: number | boolean): number | boolean {
+//   return val;
+// }
+
+// function identityTwo(val: any): any {
+//   return val;
+// }
+
+// function identityThree<T>(val: T): T {
+//   return val;
+// }
+
+// interface Bottle {
+//   brand: string;
+//   type: string;
+// }
+
+// // You can pass on your own Types in Generics
+
+// const objbottle: Bottle = {
+//   brand: "sdf",
+//   type: "sdf",
+// };
+
+// identityThree<Bottle>(objbottle);
+
+// Array in Generics
+
+function myArray<T>(val: T[]): T {
+  return val[3];
+}
+
+// Generics in Arrow functions
+
+const yourArray = <T>(val: T): T => {
   return val;
-}
-
-function identityTwo(val: any): any {
-  return val;
-}
-
-function identityThree<T>(val: T): T {
-  return val;
-}
-
-interface Bottle {
-  brand: string;
-  type: string;
-}
-
-// You can pass on your own Types in Generics
-
-const objbottle: Bottle = {
-  brand: "sdf",
-  type: "sdf",
 };
-
-identityThree<Bottle>(objbottle);
